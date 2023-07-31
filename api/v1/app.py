93c38a8fcf17a10ae09cf88091215743d3f52402
chriss1525 <okothchris15@gmail.com>
2023-08-01 00:19:35 +0300

    add readme

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,21 +8,26 @@ from flask import Flask, Blueprint, jsonify
 from flask_cors import CORS
 import os
 
+"""initializes a new Flask app"""
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+
 
 @app.teardown_appcontext
 def teardown_appcontext(self):
     """teardown_appcontext"""
     storage.close()
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     """page_not_found"""
     return jsonify({"error": "Not found"}), 404
 
+
 if __name__ == "__main__":
-   app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', '5000')), 
-           threaded=True)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')),
+            threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,8 @@ from api.v1.views import app_views
 from flask import jsonify, request, abort
 from models import storage
 
+"""initializes a new route"""
+
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
