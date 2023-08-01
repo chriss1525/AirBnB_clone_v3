f9f24b65fad0aef1f18fd0489a3309dd0904d688
chriss1525 <okothchris15@gmail.com>
2023-08-01 13:54:21 +0300

    add api end routes for states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -63,3 +63,18 @@ def create_state():
         new_state = State(**request.get_json())
         new_state.save()
         return jsonify(new_state.to_dict()), 201
+
+# route to update a state
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    """Updates a State object"""
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    for key, value in request.get_json().items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(state, key, value)
+    state.save()
+    return jsonify(state.to_dict()), 200
