2148a9752c9d3780d505f93fb9cfd5216fb1e1ea
chriss1525 <okothchris15@gmail.com>
2023-08-01 10:12:35 +0300

    documentation

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+"""initializes a new route"""
+
 from api.v1.views import app_views
 from flask import jsonify, request, abort
 from models import storage
-
-"""initializes a new route"""
-
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
