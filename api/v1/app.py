adfa45a3d1854aa2a564a3e4abc70eba9a041696
chriss1525 <okothchris15@gmail.com>
2023-08-01 10:45:42 +0300

    import index and all its routes inside views

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,11 +9,12 @@ from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify
 from flask_cors import CORS
 import os
+from os import getenv
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
@@ -29,6 +30,12 @@ def page_not_found(e):
 
 
 if __name__ == "__main__":
-    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', '5000')),
-            threaded=True)
+    if getenv("HBNB_API_HOST") is None:
+        HBNB_API_HOST = '0.0.0.0'
+    else:
+        HBNB_API_HOST = getenv("HBNB_API_HOST")
+    if getenv("HBNB_API_PORT") is None:
+        HBNB_API_PORT = 5000
+    else:
+        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
